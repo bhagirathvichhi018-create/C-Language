725bb25e59d464f14ef76f5d8052b2d7b617a328
Bhagirath Vichhi <bhagirathvichhi018@gmail.com>
2026-04-23 18:59:19 +0530

    Add files via upload

---
+++ ch-6/1-1/age.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ main()
+ {
+ 	
+   int age;
+   printf("Enter your Age\t:");
+   scanf("%d", &age); 	
+ 	
+ 	if(age >= 18)
+ 	{
+ 		printf("You can VOTE");
+ 		
+ 		
+	 }
+     else
+     {
+     	printf("You can not VOTE");
+	 }
+ }
